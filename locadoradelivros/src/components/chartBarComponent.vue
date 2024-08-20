98b4c50f48b05a68f40c7c07943fc9609cc78b11
João Pedro Mota <joaopedromota67@gmail.com>
2024-08-20 15:01:46 -0300

    final commit

---
+++ locadoradelivros/src/components/chartBarComponent.vue
@@ -0,0 +1,87 @@
+<template>
+  <div style="width: 100%; height: 35vh;">
+    <div id="title">Relações de livros</div>
+    <canvas :id="canvasId"></canvas>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, defineProps } from 'vue';
+import { Chart, registerables } from 'chart.js';
+import { api, authenticate } from 'src/boot/axios';
+
+Chart.register(...registerables);
+
+const props = defineProps({
+  canvasId: {
+    type: String,
+    required: true
+  }
+});
+
+const rentsQtd = ref(0);
+const inTime = ref(0);
+const delivered = ref(0);
+const delayed = ref(0);
+
+const fetchData = async () => {
+  try {
+    await authenticate();
+
+    const totalResponse = await api.get('/rent');
+    rentsQtd.value = totalResponse.data.totalElements;
+
+    const inTimeResponse = await api.get('/rent', { params: { status: 'IN_TIME' } });
+    inTime.value = inTimeResponse.data.totalElements;
+
+    const deliveredResponse = await api.get('/rent', { params: { status: 'DELIVERED' } });
+    delivered.value = deliveredResponse.data.totalElements;
+
+    const delayedResponse = await api.get('/rent', { params: { status: 'DELAYED' } });
+    delayed.value = delayedResponse.data.totalElements;
+
+  } catch (error) {
+    console.error("Erro ao obter dados:", error);
+  }
+};
+
+onMounted(async () => {
+  await fetchData();
+
+  const ctx = document.getElementById(props.canvasId).getContext('2d');
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Alugados', 'Atrasados', 'No prazo', 'Fora do prazo'],
+      datasets: [{
+        label: '',
+        data: [rentsQtd.value, delayed.value, inTime.value, delivered.value],
+        backgroundColor: ['rgb(34, 1, 39)', 'rgb(217, 2, 9)', 'rgb(54, 162, 235)',  'rbg(217, 2, 9)'],
+        borderWidth: 0,
+        borderRadius: 0
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+});
+</script>
+
+<style scoped>
+
+.title {
+  font-weight: bold;
+}
+
+canvas {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ locadoradelivros/src/pages/IndexPage.vue
@@ -0,0 +1,66 @@
+<template>
+  <q-page padding>
+    <div class="graphics-top" style="width: 100%;">
+      <div class="doughnut" style="width: 50%; height: 40vh; box-shadow: black;">
+        <chart-bar-component canvasId="relacoesLivrosChart"></chart-bar-component>
+      </div>
+      <div class="doughnut" style="width: 50%; height: 40vh; box-shadow: black;">
+        <chart-doughnut-component canvasId="BooksChart"></chart-doughnut-component>
+      </div><br/>
+    </div>
+    <div class="tb" style="width: 100%; box-shadow: black;">
+      <div id="title">Top 3 locatários mais ativos</div>
+      <TableComponents :columns="columns" :rows="rows">
+      </TableComponents>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import TableComponents from '../components/TableComponents.vue';
+import chartBarComponent from '../components/chartBarComponent.vue';
+import chartDoughnutComponent from 'src/components/chartDoughnutComponent.vue';
+import { api, authenticate } from 'src/boot/axios';
+
+onMounted(() => {
+  authenticate()
+    .then(() => {
+      console.log("Sucesso");
+      getTable();
+    })
+    .catch(error => {
+      console.error("Erro na autenticação", error);
+    });
+});
+
+const columns = [
+  { name: 'id', align: 'center', label: 'Locatário', field: 'id' },
+  { name: 'name', align: 'center', label: 'Livro', field: 'name' },
+  { name: 'totalRents', align: 'center', label: 'Alugado', field: 'totalRents' },
+  { name: 'activeRents', align: 'center', label: 'Devolução', field: 'activeRents' },
+];
+
+const rows = ref([]);
+
+const getTable = async () => {
+  try {
+    await authenticate();
+
+    const response = await api.get('/rent/renters');
+
+    if (Array.isArray(response.data.content)) {
+      rows.value = response.data.content
+        .sort((a, b) => b.totalRents - a.totalRents)
+        .slice(0, 3);
+      console.log("Dados obtidos com sucesso");
+    } else {
+      console.error('A resposta da API não é um array:', response.data);
+      rows.value = [];
+    }
+    console.log('Resposta da API:', response.data);
+  } catch (error) {
+    console.error("Erro ao obter dados:", error);
+  }
+};
+</script>
